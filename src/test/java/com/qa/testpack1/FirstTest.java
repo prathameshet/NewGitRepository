7abd0505897ebd64354ba513859e5c16fb1b6334
prathameshet <prathameshetc@gmail.com>
2021-12-03 19:43:10 +0530

    Indian Time Zone

--- src/test/java/com/qa/testpack1/FirstTest.java
+++ src/test/java/com/qa/testpack1/FirstTest.java
@@ -7,15 +7,18 @@ public class FirstTest {
 	@Test
 	public void Test1() {
 		System.out.println("Test1");
+		System.out.println("TestingTest1");
 	}
 	
 	@Test
 	public void Test2() {
 		System.out.println("Test2");
+		System.out.println("TestingTest2");
 	}
 	
 	@Test
 	public void Test3() {
 		System.out.println("Test3");
+		System.out.println("TestingTest3");
 	}
 }
